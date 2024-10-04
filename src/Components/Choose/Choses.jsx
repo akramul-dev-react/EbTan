23df8b821a60d4d7534ea39c3e549ac63f860e2b
akramul2020 <akramul2512@gmail.com>
2024-10-04 09:42:54 +0600

    chose part add

---
+++ src/Components/Choose/Choses.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Choses = () => {
+  return (
+   <>
+   <section>
+    <div className="container">
+        <div className="chos_main_curt">
+            <div className="chosheding flex flex-col  items-center">
+                <h1 className='text-[48px] font-normal text-secend_color font-serif'>Why choose us</h1>
+                <p className='w-[526px] text-[18px] font-medium text-[#A0A79A] font-sans '>
+                SLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+                </p>
+            </div>
+        </div>
+    </div>
+    </section></>
+  )
+}
+
+export default Choses
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Baner from '../Components/Baner/Baner'
 import Navbar from '../Components/Navbar/Navbar'
+import Choses from '../Components/Choose/Choses'
 
 const Home = () => {
   return (
   <>
   <Baner></Baner>
+  <Choses></Choses>
+ 
   </>
    
   )
